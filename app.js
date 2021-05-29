530395a5af93312751560f931f3f07d840d3bbcd
Sara-Garcia-Palacios <palac5691@gmail.com>
2021-05-29 01:57:15 -0500

    examen

--- app.js
+++ app.js
@@ -13,34 +13,43 @@
   firebase.initializeApp(firebaseConfig);
 
   //obtener datos del html
-
   var txtUsuario = document.getElementById("usuario");
   var txtMensaje = document.getElementById("mensaje");
   var btnEnviar = document.getElementById("btnenviar");
   var chatlista = document.getElementById("chatlista");
+  var fechahora = new Date();
+  var fecha = fechahora.toString();
+  fecha = fecha.substring(0,24);
+
 
   //Ejecutar accion en el boton
   btnEnviar.addEventListener("click",function(){
     var usuario = txtUsuario.value;
     var mensaje = txtMensaje.value;
-    var html = "<li>"+usuario+" dice: "+mensaje+"</li>";
+    var html = "<li>"+usuario+" DICE: "+mensaje+" "+fecha+"</li>";
 
     chatlista.innerHTML += html;
 
     firebase.database().ref('chat').push({
         user: usuario,
-        message: mensaje
+        message: mensaje,
+        datetime:fecha
     })
   });
+
   /*Mostrar datos*/
   firebase.database().ref('chat').on('value', (snapshot) => {
     var html1 = '';
+    
     //console.log(snapshot.val());
     snapshot.forEach(function (e){
       var elemento = e.val();
       var usuario1 = elemento.user;
       var mensaje1 = elemento.message;
-      html1 += "<li>"+usuario1+" dice: "+mensaje1+"</li>";
+      var fecha1 = elemento.datetime;
+      //html1 += "<li>"+usuario1+" DICE: "+mensaje1+" "+fecha1+"</li>";
+      html1 += '<div class="chat-bubble"><div class="chat-bubble-arrow-border"></div><div class="chat-bubble-arrow"></div><h4>'+usuario1+" dice: "+mensaje1+" "+fecha1+"</h4></div>";
+   
     });
     chatlista.innerHTML = html1;
   })
